cf50e1fca6234b3980759c83d92dda5092b80092
sabbirHossain <sabbirshnt@gmail.com>
2025-11-17 14:28:13 +0600

    initialization

---
+++ src/assets/images/index.js
@@ -0,0 +1,37 @@
+import logo from "../img/logo.png";
+import india from "../img/in.svg";
+import defaultNotification from "../img/default_notification.svg";
+import warning from "../img/warning-yellow.svg";
+import gamblingCommission from "../img/gamblingCommission.svg";
+import cograE from "../img/cograE.svg";
+import BeGambleAware from "../img/BeGambleAware.svg";
+import bankTransfer from "../img/bankTransfer.svg";
+import upiIcon from "../img/upiIcon.svg";
+import cc from "../img/gaming-1.webp";
+import instagram from "../img/instagram.png";
+import whatsApp from "../img/whatsapp.png";
+import kabbadi from "../img/kabbadi.svg";
+import whatsApp2 from "../img/1384023.png";
+import appStar from "../img/app-star.svg";
+import playStore from "../img/playstore.svg";
+import bhim from "../img/bhim.png";
+
+export default {
+  bhim,
+  playStore,
+  appStar,
+  whatsApp,
+  whatsApp2,
+  instagram,
+  cc,
+  logo,
+  india,
+  defaultNotification,
+  warning,
+  gamblingCommission,
+  cograE,
+  BeGambleAware,
+  bankTransfer,
+  upiIcon,
+  kabbadi,
+};
